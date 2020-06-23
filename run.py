a238ac27a332b92a53c9e12049f5efb93a2487d3
ianianing <35719055+ianianing@users.noreply.github.com>
2020-06-23 01:39:57 -0700

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-# create this app as a package
-from flaskblog import create_app
-# app variable need to exist in __init__.py file under flaskblog folder
-
-app = create_app()
-
-if __name__ == '__main__':
-	app.run(debug=True)   #as long as import, app run debug mode
-
