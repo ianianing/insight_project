088f4297ffc459455736e0a377410b962bcb3dbd
ianianing <35719055+ianianing@users.noreply.github.com>
2020-06-23 02:41:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scipy.spatial import distance
 
 app = Flask(__name__)
-sneaker = pickle.load(open('/home/ianyin/桌面/Insight_Project/sneaker.pkl', 'rb'))
+sneaker = pickle.load(open('sneaker.pkl', 'rb'))
 
 ######################routes#################################################################
 
